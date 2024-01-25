d1c15d7934f609fc5e062270d8d047479a0cf230
vikas ghdoke <vikasghodke468@gmail.com>
2024-01-25 16:02:12 +0530

    First commit

---
+++ Class1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Array_Ex_6
+{
+    internal class Class1
+    {
+        public void sortDesc(int[] numbers)
+        {
+            for(int i = 0; i < numbers.Length; i++)
+            {
+                Console.WriteLine(numbers[i]);
+            }
+
+            Console.WriteLine("After sorting");
+            int temp;
+            for(int i=0;i<numbers.Length;i++)
+            {
+                for(int j = i; j < numbers.Length - 1; j++)
+                {
+                    if (numbers[i] < numbers[j])
+                    {
+                        temp = numbers[i];
+                        numbers[i] = numbers[j];
+                        numbers[j] = temp;
+
+                    }
+                }
+
+                Console.WriteLine("Elements=" + numbers[i]);
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Array_Ex_6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = { 23, 45, 67, 65, 43, 67 };
+
+            Class1 c1=new Class1();
+            c1.sortDesc(numbers);
+
+           
+        
+        }
+    }
+}
