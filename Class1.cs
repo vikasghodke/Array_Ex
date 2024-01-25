b68ffb29fa30c2841815a86f8c355ddd1388a3bb
vikas ghdoke <vikasghodke468@gmail.com>
2024-01-25 14:15:26 +0530

    Second commit

---
+++ Class1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Arrray_Ex_2
+{
+    internal class Class1
+    {
+        public void SumArray(int[] numbers)
+        {
+            int sum = 0;
+            for(int i = 0; i < numbers.Length; i++)
+            {
+                
+                sum += numbers[i];
+            }
+            Console.WriteLine("Sum of the Array=" + sum);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Arrray_Ex_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = { 12, 34, 56, 54, 32 };
+
+            Class1 c1 = new Class1();
+            c1.SumArray(numbers);
+        }
+    }
+}
