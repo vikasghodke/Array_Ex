8411db5132533cfe1282b23763b4cd2c4853764b
vikas ghdoke <vikasghodke468@gmail.com>
2024-01-25 14:06:49 +0530

    First commit

---
+++ Class1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Array_EX_1
+{
+    internal class Class1
+    {
+        public void RotateLeft(int[] numbers, int a, int b)
+        {
+            int[] temp = new int[a];
+
+           
+            var k = 0;
+            for(int i=b; i < a; i++)
+            {
+                temp[k] = numbers[i];
+                k++;
+            }
+            
+            for(int i = 0; i < a; i++)
+            {
+                temp[k] = numbers[i];
+                k++;
+            }
+
+            for(int i=0;i<a;i++)
+            {
+                numbers[i] = temp[i];
+            }
+           
+        }
+        public void PrintArray(int[] numbers, int a)
+        {
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                {
+                    Console.WriteLine(numbers[i].ToString());
+
+                }
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Array_EX_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = { 12, 34, 5, 6, 7, 8, 654, 35 };
+            var b = numbers.Length;
+            var a = 5;
+
+            Class1 c1 = new Class1();
+            c1.RotateLeft(numbers, a,b);
+            c1.PrintArray(numbers, a);
+        }
+    }
+}
