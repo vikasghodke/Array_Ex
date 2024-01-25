f3a80ee67cac4c713a45f10caa705e6949acecd3
vikas ghdoke <vikasghodke468@gmail.com>
2024-01-25 14:35:19 +0530

    forth commit

---
+++ Class1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Array_Ex_4
+{
+    internal class Class1
+    {
+        public void ReverseArray(int[] numbers)
+        {
+            for(int i = 0; i < numbers.Length; i++)
+            {
+                Console.WriteLine(numbers[i]);
+            }
+            Array.Reverse(numbers);
+            foreach(int value in  numbers){
+                Console.WriteLine("Reversed Array" +value);
+
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Array_Ex_4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = { 23, 34, 56, 68, 66, 54, 32 };
+
+            Class1 c1 = new Class1();
+
+            c1.ReverseArray(numbers);
+        }
+    }
+}
