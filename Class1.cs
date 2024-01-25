a294b9b2e45102661873c2a548e7cd7285eaac9c
vikas ghdoke <vikasghodke468@gmail.com>
2024-01-25 14:23:50 +0530

    Third commit

---
+++ Class1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Array_Ex_3
+{
+    internal class Class1
+    {
+        public void SortDesc(int[] numbers)
+        {
+            int temp;
+            for(int i = 0; i < numbers.Length; i++)
+            {
+                for(int j = i; j < numbers.Length - 1; j++)
+                {
+                    if (numbers[i] > numbers[j])
+                    {
+                        temp = numbers[i];
+                        numbers[i] = numbers[j];
+                        numbers[j] = temp;
+
+                    }
+                }
+                
+            }
+        }
+        public void SortedArray(int[] numbers)
+        {
+            for(int i=0;i<numbers.Length;i++)
+            {
+                Console.WriteLine(numbers[i]);
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Array_Ex_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = { 23, 45, 43, 65, 67, 45, 67 };
+
+            Class1 c1 = new Class1();
+
+            c1.SortDesc(numbers);
+            c1.SortedArray(numbers);
+        }
+    }
+}
